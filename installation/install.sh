b57751c80fac73ba5e52517ae51b520ec7e73700
marlonriv <Marlonriv90@gmail.com>
2025-04-24 15:36:23 +0200

    added installation of webrtc from unstable ppa

--- installation/install.sh
+++ installation/install.sh
@@ -5,4 +5,5 @@ MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 # install dependencies
 sudo apt-get install ros-noetic-async-web-server-cpp 
-
+curl https://ctu-mrs.github.io/ppa-unstable/add_ppa.sh | bash
+sudo apt-get install ros-noetic-webrtc
